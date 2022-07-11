0dd7709f8cdf4c7e3026a436654e04986a92dba4
Muhammed Abiola <hi@mabiola.net>
2022-07-11 09:57:10 +0100

    Refashioning back-end database using Mongoose

---
+++ server/config/config.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV !== "production" ? require("dotenv").config() : false;
+
+const config = {
+  env: process.env.NODE_ENV ?? "development",
+  port: process.env.PORT ?? 3000,
+  jwtSecret: process.env.JWT_SECRET ?? "SECRET_KEY",
+  maison:
+    process.env.MONGO_URI ??
+    process.env.MONGO_HOST ??
+    "mongodb://" +
+      (process.env.IP ?? "localhost") +
+      ":" +
+      (process.env.MONGO_PORT ?? "27017") +
+      "/maison",
+};
+
+export default config;
